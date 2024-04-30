c2b3e451acd09b82cfb107449013cf31a60f9341
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-30 17:00:48 +0200

    control added

--- snack4.php
+++ snack4.php
@@ -1,10 +1,12 @@
 <?php
-$randomnumbers =[];
+$randomnumbers = [];
 
-for($i = 0; $i < 15; $i++){
-    $casualnumber = rand(1,100);
-    $randomnumbers[] = $casualnumber ;
+for ($i = 0; $i < 15; $i++) {
+    $casualnumber = rand(1, 100);
+    if (!in_array($casualnumber, $randomnumbers)) {
+        $randomnumbers[] = $casualnumber;
+    }
 }
 
 var_dump($randomnumbers)
-?>
+    ?>
