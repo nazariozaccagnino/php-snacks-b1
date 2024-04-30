3813769f8f92a76bc9efe1a09cac497ce92dda09
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-30 16:23:46 +0200

    snack 3 completed without style

---
+++ snack3.php
@@ -0,0 +1,79 @@
+<?php
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Snack 3</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Social Posts</h1>
+        <div>
+            <div class="row">
+                <?php foreach($posts as $key => $post){?>
+
+                    <div class="col">
+                        <?php echo $key?>
+                    <?php foreach($post as $text) {?>
+
+                        <div><?php echo $text['title']?></div>
+                        <div><?php echo $text['author']?></div>
+                        <div><?php echo $text['text']?></div>
+
+                        <?php }?>
+                </div>
+                <?php }?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
